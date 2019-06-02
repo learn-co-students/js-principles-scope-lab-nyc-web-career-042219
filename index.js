42dba1ea9a2f8565c3bcbf671f4de6487fcb2dbc
cbatsoula <cbatsoula@gmail.com>
2019-06-02 17:44:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 var customerName = 'bob'
-const leastFavoriteCustomer = 'some initial value'
+const leastFavoriteCustomer = 'some value'
 var bestCustomer;
 
 function upperCaseCustomerName() {
